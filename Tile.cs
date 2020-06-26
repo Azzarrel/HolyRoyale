b325de7d675f3f5bc66b1387068054e9b878bb48
Zariteis <core@zariteis.de>
2020-06-26 23:59:28 +0200

    Tile Formating

--- Tile.cs
+++ Tile.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,28 +11,28 @@ namespace HolyRoyal
   public class Tile 
   {
 
-    public Vector2 Position { get; set; }
+	public Vector2 Position { get; set; }
 
-    public TileType Type { get; set; }
+	public TileType Type { get; set; }
 
-    public IList<Feature> Features { get; set; }
+	public IList<Feature> Features { get; set; }
 
-    public Tile(Vector2 pos, TileType tileType, IList<Feature> features)
-    {
-      Position = pos;
-      Type = tileType;
-      Features = features;
-    }
+	public Tile(Vector2 pos, TileType tileType, IList<Feature> features)
+	{
+	  Position = pos;
+	  Type = tileType;
+	  Features = features;
+	}
 
-    public int GetSlots()
-    {
-      int slots = 1;
-      foreach(Feature f in Features)
-      {
-        slots += f.Slots;
-      }
-      return slots;
-    }
+	public int GetSlots()
+	{
+	  int slots = 1;
+	  foreach(Feature f in Features)
+	  {
+		slots += f.Slots;
+	  }
+	  return slots;
+	}
 
   }
 }
